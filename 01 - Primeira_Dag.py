f1be7f643c85402e5513892a851798c0c031599e
CJAOliveira <85760730+CJAOliveira@users.noreply.github.com>
2023-04-04 20:15:09 -0400

    Update 01 - Primeira_Dag.py

--- 01 - Primeira_Dag.py
+++ 01 - Primeira_Dag.py
@@ -5,19 +5,38 @@ import os
 import subprocess
 
 
-def execute_my_script():
+def execute_my_script_01():
     script_path = os.path.join(
-        os.environ['AIRFLOW_HOME'], 'scripts', 'ingestao_dados.py')
+        os.environ['AIRFLOW_HOME'], 'scripts', 'importar_arquivos.py')
     subprocess.call(['python', script_path])
     print("funciona")
 
 
-dag = DAG('INGESTAO_DADOS_EXAMES', description='NORMALIZAR_DADOS',
-          schedule_interval='0 12 * * *',
+def execute_my_script_02():
+    script_path = os.path.join(
+        os.environ['AIRFLOW_HOME'], 'scripts', 'normatizacao_dados.py')
+    subprocess.call(['python', script_path])
+    print("funciona")
+
+
+def execute_my_script_03():
+    script_path = os.path.join(
+        os.environ['AIRFLOW_HOME'], 'scripts', 'carga_dw.py')
+    subprocess.call(['python', script_path])
+    print("funciona")
+
+
+dag = DAG('INGESTAO_DADOS_EXAMES_2', description='NORMALIZAR_DADOS',
+          schedule_interval='0 */2 * * *',
           start_date=datetime(2023, 1, 1), catchup=False)
 
-task = PythonOperator(
-    task_id='my_task', python_callable=execute_my_script, dag=dag)
+importar_dados_raw = PythonOperator(
+    task_id='importar_dados_raw', python_callable=execute_my_script_01, dag=dag)
 
+normatizar_dados = PythonOperator(
+    task_id='normatizar_dados', python_callable=execute_my_script_02, dag=dag)
 
-task
+carga_dw_datalake = PythonOperator(
+    task_id='carga_dw_datalake', python_callable=execute_my_script_03, dag=dag)
+
+importar_dados_raw >> normatizar_dados >> carga_dw_datalake
